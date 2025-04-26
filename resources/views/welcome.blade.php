b75092791e49e97d3b0cee1c6e2fe8fba42c2d1b
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2025-04-26 12:11:42 +0700

    rev: coba notif tele

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -943,6 +943,7 @@
 
                 <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
                     Api Jangurkeramik v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                    <!-- coba perubahan notif -->
                 </div>
             </div>
         </div>
