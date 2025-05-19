297c924bfaa85ead7906674c160014282573189c
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2025-05-19 15:26:01 +0700

    rev: good

--- app/Http/Controllers/Api/v2/Master/MasterController.php
+++ app/Http/Controllers/Api/v2/Master/MasterController.php
@@ -20,10 +20,23 @@ class MasterController extends Controller
     {
         $query = User::query();
 
-        $query->select('id', 'nama', 'email', 'nohp', 'alamat','avatar')
+        $query->select('id', 'nama', 'email', 'nohp', 'alamat','avatar','username','jabatan')
             ->where('kodejabatan', 3)
             ->orderBy('nama', 'ASC');
         $sales = $query->get();
+        return response()->json($sales);
+    }
+    public function pelanggan()
+    {
+        $query = User::query();
+
+        $query->select('id', 'nama', 'email', 'nohp', 'alamat','avatar','username','jabatan')
+            ->when(request('q'), function ($query) {
+                $query->where('nama', 'like', '%' . request('q') . '%');
+            })
+            ->whereNull('kodejabatan')
+            ->orderBy('nama', 'ASC');
+        $sales = $query->limit(20)->get();
         return response()->json($sales);
     }
 }
