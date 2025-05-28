dbd722289ee293c20066e5fffdade2507514a54d
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2025-05-29 01:47:04 +0700

    rev: v2 cart

--- routes/v2/cart/cart.php
+++ routes/v2/cart/cart.php
@@ -3,7 +3,18 @@
 use App\Http\Controllers\Api\v2\Product\CartController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->prefix('cart')->group(function () {
+// Route::middleware('auth:sanctum')->prefix('cart')->group(function () {
+//     Route::get('/', [CartController::class, 'index']);
+//     Route::post('/', [CartController::class, 'store']);
+//     Route::delete('/destroy-all', [CartController::class, 'destroyAllCart']);
+//     Route::put('/{barang}', [CartController::class, 'update']);
+//     Route::delete('/{barang}', [CartController::class, 'destroy']);
+// });
+
+Route::group([
+    'prefix' => 'cart',
+    'middleware' => ['web', 'auth:sanctum'],
+], function () {
     Route::get('/', [CartController::class, 'index']);
     Route::post('/', [CartController::class, 'store']);
     Route::delete('/destroy-all', [CartController::class, 'destroyAllCart']);
