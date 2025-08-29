ed882dafc053846dc76832580b875313bab7505e
pigartpigaro <vichoirul@gmail.com>
2025-08-30 00:13:13 +0700

    rev: kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,13 +14,13 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('stok:opname')->dailyAt('00:30');
-        $schedule->command('sync:barang-views')->everyFiveMinutes();
+        // $schedule->command('sync:barang-views')->everyFiveMinutes();
         // $schedule->command('sync:barang-views')->everyMinute();
         $schedule->command('sync:barang-likes')->everyFiveMinutes();
 
         // $schedule->command('stock:opnamelaporan')->monthly()->at('23:59');
         $schedule->command('stock:opnamelaporan')->monthlyOn(1, '00:10');
-        $schedule->command('stock:percobaan')->dailyAt( '00:10');
+        // $schedule->command('stock:percobaan')->dailyAt( '00:10');
     }
 
     /**
