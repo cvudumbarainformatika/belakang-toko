3fc6265d0b154bfafaa4c61995e6d40a56fe7154
pigartpigaro <vichoirul@gmail.com>
2025-09-05 00:34:54 +0700

    rev: laporan ulang

--- app/Http/Controllers/Api/Laporan/LaporanAkuntansiController.php
+++ app/Http/Controllers/Api/Laporan/LaporanAkuntansiController.php
@@ -30,16 +30,107 @@ class LaporanAkuntansiController extends Controller
             ->groupBy('akun', 'keterangan')
             ->selectRaw('akun, keterangan, SUM(nilai) as total')
             ->get();
+        if ($arusKasMasuk->isEmpty()) {
+            $startOfMonth = Carbon::parse($from)->startOfMonth()->format('Y-m-d');
+            $endOfMonth   = Carbon::parse($to)->endOfMonth()->format('Y-m-d');
+
+            // Penjualan Lunas
+            $totalLunas = DB::table('header_penjualans')
+                ->where('flag', 5)
+                ->whereBetween('tgl', [$startOfMonth . ' 00:00:00', $endOfMonth . ' 23:59:59'])
+                ->leftJoin('detail_penjualans', 'detail_penjualans.no_penjualan', '=', 'header_penjualans.no_penjualan')
+                ->sum('detail_penjualans.subtotal');
+
+            // Penjualan Menggunakan DP (flag = 7)
+            $totalDP = DB::table('header_penjualans')
+                ->where('flag', 7)
+                ->whereBetween('tgl', [$startOfMonth . ' 00:00:00', $endOfMonth . ' 23:59:59'])
+                ->sum('bayar');
+
+            $totalCicilan = DB::table('pembayaran_cicilans')
+                    ->whereBetween('tgl_bayar', [$startOfMonth . ' 00:00:00', $endOfMonth . ' 23:59:59'])
+                    ->sum('jumlah');
+            $totalPendapatan = $totalLunas + $totalDP;
+
+            // kalau arusKasMasuk kosong → fallback ke pendapatan penjualan
+            $totalMasuk = $totalPendapatan + $totalCicilan;
+            $arusKasMasuk = [
+                [
+                    'akun' => '0001-IN',
+                    'keterangan' => 'Pendapatan Penjualan',
+                    'total' => $totalPendapatan
+                ],
+                [
+                    'akun' => '0002-IN',
+                    'keterangan' => 'Pendapatan dari Cicilan',
+                    'total' => $totalCicilan
+                ]
+            ];
+        } else {
+            // kalau ada data normal
+            $totalMasuk = $arusKasMasuk->sum('total');
+        }
+
 
         // Kas keluar
         $arusKasKeluar = OpnamePengeluaran::whereBetween('tgl_opname', [$from, $to])
             ->groupBy('akun', 'keterangan')
             ->selectRaw('akun, keterangan, SUM(nilai) as total')
             ->get();
+        if ($arusKasKeluar->isEmpty()) {
+            $startOfMonth = Carbon::parse($from)->startOfMonth()->format('Y-m-d');
+            $endOfMonth   = Carbon::parse($to)->endOfMonth()->format('Y-m-d');
 
-        // Total masuk & keluar
-        $totalMasuk = $arusKasMasuk->sum('total');
-        $totalKeluar = $arusKasKeluar->sum('total');
+            $pembelianBarang = DB::table('penerimaan_h')
+                    ->where('kunci', 1)
+                    ->where('jenis_pembayaran', 'Cash')
+                    ->whereBetween('tgl_faktur', [$startOfMonth, $endOfMonth])
+                    ->leftJoin('penerimaan_r', 'penerimaan_r.nopenerimaan', '=', 'penerimaan_h.nopenerimaan')
+                    ->sum('penerimaan_r.subtotalfix');
+
+            $bebankeluar = DB::table('transbeban_headers')
+                    ->where('flaging', 1)
+                    ->whereBetween('tgl', [$startOfMonth, $endOfMonth])
+                    ->leftJoin('transbeban_rincis', 'transbeban_rincis.notrans', '=', 'transbeban_headers.notrans')
+                    ->sum('transbeban_rincis.subtotal');
+            $returpenjualan = DB::table('header_retur_penjualans')
+                    ->where('status', 1)
+                    ->whereBetween('tgl', [$startOfMonth . ' 00:00:00', $endOfMonth . ' 23:59:59'])
+                    ->leftJoin('detail_retur_penjualans', 'detail_retur_penjualans.header_retur_penjualan_id', '=', 'header_retur_penjualans.id')
+                    ->sum('detail_retur_penjualans.subtotal');
+            $Pembayaranhutang = DB::table('pembayaran_hutang_h')
+                    ->whereBetween('tgl_bayar', [$startOfMonth, $endOfMonth])
+                    ->leftJoin('pembayaran_hutang_r', 'pembayaran_hutang_r.notrans', '=', 'pembayaran_hutang_h.notrans')
+                    ->sum('pembayaran_hutang_r.total');
+
+            $totalKeluar = $pembelianBarang + $bebankeluar + $returpenjualan + $Pembayaranhutang;
+            $arusKasKeluar = [
+                [
+                    'akun' => '0001-OUT',
+                    'keterangan' => 'Pembelian Langsung',
+                    'total' => $pembelianBarang
+                ],
+                [
+                    'akun' => '0002-OUT',
+                    'keterangan' => 'Beban Pengeluaran',
+                    'total' => $bebankeluar
+                ],
+                [
+                    'akun' => '0003-OUT',
+                    'keterangan' => 'Retur Penjualan',
+                    'total' => $returpenjualan
+                ],
+                [
+                    'akun' => '0004-OUT',
+                    'keterangan' => 'Pembayaran Hutang',
+                    'total' => $Pembayaranhutang
+                ]
+            ];
+        } else {
+                    // kalau ada data normal
+                    $totalKeluar = $arusKasKeluar->sum('total');
+                }
+
 
         $kenaikanKas = $totalMasuk - $totalKeluar;
         $saldoAkhir = $saldoAwal + $kenaikanKas;
@@ -100,7 +191,26 @@ class LaporanAkuntansiController extends Controller
             ->groupBy('akun', 'keterangan')
             ->selectRaw('akun, keterangan, SUM(nilai) as total')
             ->get();
-        $totalBeban = $beban->sum('total');
+          if ($beban->isEmpty()) {
+            $startOfMonth = Carbon::parse($from)->startOfMonth()->format('Y-m-d');
+            $endOfMonth   = Carbon::parse($to)->endOfMonth()->format('Y-m-d');
+            $bebankeluar = DB::table('transbeban_headers')
+                    ->where('flaging', 1)
+                    ->whereBetween('tgl', [$startOfMonth, $endOfMonth])
+                    ->leftJoin('transbeban_rincis', 'transbeban_rincis.notrans', '=', 'transbeban_headers.notrans')
+                    ->sum('transbeban_rincis.subtotal');
+            $totalBeban = $bebankeluar;
+            $beban = [
+                    [
+                        'akun' => '0002-OUT',
+                        'keterangan' => 'Beban Pengeluaran',
+                        'total' => $bebankeluar
+                    ],
+                ];
+            } else {
+                $totalBeban = $beban->sum('total');
+          }
+
         $labaoperasional = $labaKotor - $totalBeban;
         return response()->json([
             // 'persediaan_awal' => $persediaanAwal,
