eb7ecd00c2cceb231b1934da8070428472e16f3b
pigartpigaro <vichoirul@gmail.com>
2025-09-05 19:46:56 +0700

    rev: hpp pindah ke detail penjualan fifo

--- app/Http/Controllers/Api/Laporan/LaporanAkuntansiController.php
+++ app/Http/Controllers/Api/Laporan/LaporanAkuntansiController.php
@@ -160,8 +160,8 @@ class LaporanAkuntansiController extends Controller
 
         $hpp =  DB::table('header_penjualans')
             ->whereBetween('tgl', [$from . ' 00:00:00', $to . ' 23:59:59'])
-            ->leftJoin('detail_penjualans', 'detail_penjualans.no_penjualan', '=', 'header_penjualans.no_penjualan')
-            ->sum(DB::raw('(detail_penjualans.jumlah * detail_penjualans.harga_beli)'));
+            ->leftJoin('detail_penjualan_fifos', 'detail_penjualan_fifos.no_penjualan', '=', 'header_penjualans.no_penjualan')
+            ->sum(DB::raw('(detail_penjualan_fifos.jumlah * detail_penjualan_fifos.harga_beli)'));
 
         if ($penjualan->isEmpty()) {
             $startOfMonth = Carbon::parse($from)->startOfMonth()->format('Y-m-d');
