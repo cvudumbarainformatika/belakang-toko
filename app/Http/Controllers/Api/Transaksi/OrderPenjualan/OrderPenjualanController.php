a36f0f42875b6cd81974a0ce463c71b05e4019b6
duro06 <andi.meka.025@gmail.com>
2025-11-08 22:36:59 +0700

    rev : ctt

--- app/Http/Controllers/Api/Transaksi/OrderPenjualan/OrderPenjualanController.php
+++ app/Http/Controllers/Api/Transaksi/OrderPenjualan/OrderPenjualanController.php
@@ -19,97 +19,102 @@ class OrderPenjualanController extends Controller
 
 		$data = $this->eagerLoadOrder($query)
 			->orderBy('tglorder', 'DESC')
-        	->simplePaginate(request('per_page'));
+			->simplePaginate(request('per_page'));
 
-        return new JsonResponse($data);
+		return new JsonResponse($data);
 	}
 
 	protected function eagerLoadOrder($query)
 	{
 		return $query->select(
-			'id', 'noorder', 'tglorder', 'pelanggan_id', 'sales_id', 
-			'total_harga', 'status_order', 'status_pembayaran', 
-			'tanggal_kirim', 'tanggal_terima','metode_bayar',
-			'bayar','tempo','catatan'
+			'id',
+			'noorder',
+			'tglorder',
+			'pelanggan_id',
+			'sales_id',
+			'total_harga',
+			'status_order',
+			'status_pembayaran',
+			'tanggal_kirim',
+			'tanggal_terima',
+			'metode_bayar',
+			'bayar',
+			'tempo',
+			'catatan'
 		)->with([
-			'rincians:id,order_penjualan_id,barang_id,jumlah,harga,satuan,satuans,subtotal', 
+			'rincians:id,order_penjualan_id,barang_id,jumlah,harga,satuan,satuans,subtotal',
 			'rincians.barang:id,namabarang,isi,satuan_k,satuan_b',
-			'rincians.barang.images',  
-			'pelanggan:id,nama,alamat,telepon,norek,namabank', 
+			'rincians.barang.images',
+			'pelanggan:id,nama,alamat,telepon,norek,namabank',
 			'sales:id,nama'
 		]);
-
 	}
 
 
 	public function updateRincian(Request $request)
 	{
-	   $rincian_id = $request->id;
-	   $order_id = $request->order_id;
-
-	   
-	   $rincian = OrderPenjualanRincian::find($rincian_id);
-
-	   $rincian->jumlah = $request->jumlah;
-	   $rincian->satuan = $request->satuan;
-	   $rincian->subtotal = $request->subtotal;
-
-	   $rincian->save();
-
-	   $order = OrderPenjualan::find($order_id);
-	   $order->total_harga = $request->total_harga_order;
-	   $order->save();
-
-	   $data=['message'=> 'success','order'=>$order, 'rincian'=> $rincian];
-
-	   return new JsonResponse($data);
+		$rincian_id = $request->id;
+		$order_id = $request->order_id;
 
 
+		$rincian = OrderPenjualanRincian::find($rincian_id);
+
+		$rincian->jumlah = $request->jumlah;
+		$rincian->satuan = $request->satuan;
+		$rincian->subtotal = $request->subtotal;
+
+		$rincian->save();
+
+		$order = OrderPenjualan::find($order_id);
+		$order->total_harga = $request->total_harga_order;
+		$order->save();
+
+		$data = ['message' => 'success', 'order' => $order, 'rincian' => $rincian];
+
+		return new JsonResponse($data);
 	}
 	public function deleteRincian(Request $request)
 	{
-	   $rincian_id = $request->id;
-	   $order_id = $request->order_id;
-	   
-	   
-	   $rincian = OrderPenjualanRincian::find($rincian_id);
-
-	   $rincian->delete();
-
-	   $order = OrderPenjualan::find($order_id);
-	   $order->total_harga = $request->total_harga_order;
-	   $order->save();
-
-	   
-	   $data=['message'=> 'success','order'=>$order, 'rincian'=> $rincian];
-
-	   return new JsonResponse($data);
+		$rincian_id = $request->id;
+		$order_id = $request->order_id;
 
 
+		$rincian = OrderPenjualanRincian::find($rincian_id);
+
+		$rincian->delete();
+
+		$order = OrderPenjualan::find($order_id);
+		$order->total_harga = $request->total_harga_order;
+		$order->save();
+
+
+		$data = ['message' => 'success', 'order' => $order, 'rincian' => $rincian];
+
+		return new JsonResponse($data);
 	}
 	public function updateStatus(Request $request)
 	{
-	   $order_id = $request->order_id;
+		$order_id = $request->order_id;
 
-	   $status = $request->status_order;
+		$status = $request->status_order;
 
-	   $order = OrderPenjualan::find($order_id);
-	   $order->status_order = $request->status_order;
-	   $order->save();
+		$order = OrderPenjualan::find($order_id);
+		$order->status_order = $request->status_order;
+		$order->save();
 
-	   if ($status === '5') {
-		# code... INI HARUSNYA KE PENJUALAN AKTUAL
-	   }
+		if ($status === '5') {
+			// tgl kirim di isi di header order disini, default hari ini
+			// ngisi header rincian penjualan dengan nomor penjualan = nomor order
+			// proses fifo disini seperti di funngsi simpanPembayaran() di penjualan controller
+			// flag tergantung cara bayar.. cash iku langsung 5, kalo ada dp, iku lali, lek utang, golek maneh nang kodingan
 
-	   
-	   $data=['message'=> 'success','order'=>$order];
-    	event(new SendNotificationEvent(null,'order-penjualan','order-status',$data));
-
-	   return new JsonResponse($data);
+			# code... INI HARUSNYA KE PENJUALAN AKTUAL
+		}
 
 
+		$data = ['message' => 'success', 'order' => $order];
+		event(new SendNotificationEvent(null, 'order-penjualan', 'order-status', $data));
+
+		return new JsonResponse($data);
 	}
 }
-
-
-
